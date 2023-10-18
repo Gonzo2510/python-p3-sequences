34d95b0b5b4d9e28f213baccdb38e0fdd7cb2b1a
Jacob Gonzales <jagonz@live.com>
2023-10-18 10:25:42 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length):    
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length > 1:
+        counter = length
+        sequence = [0,1]
+        while len(sequence) < counter:
+            last = sequence[-1]
+            second_last = sequence[-2]
+            next = (second_last + last)
+            sequence.append(next)
+        print(sequence)
